6e007095a2af6f9427b8232b88decadc8cc6e51f
octopus84 <octaviogodoyg@gmail.com>
2022-08-07 12:26:47 -0400

    Primer Bash

---
+++ mi-primer-bash.sh
@@ -0,0 +1,6 @@
+
+#En este primer script has introducido dos líneas. La primera línea es la que indica el intérprete que se debe utilizar. 
+#Se conoce como shebang. Es importante que recuerdes que tienes que indicar la ruta completa.
+#!/bin/bash
+
+echo "Hola Mundo"
